98ef53563c3b44d97c9f8622cf940df762f520f9
Max Hughson <hughsonm@myumanitoba.ca>
2019-09-13 16:48:17 -0500

    Replaced code with something a little more structured, to get a feel of how gmsh interprets its meshes.

--- Assignments/A1/A1.cpp
+++ Assignments/A1/A1.cpp
@@ -5,8 +5,15 @@
 
 int main (int argc, char **argv)
 {
+    assert(argc==2);
+
+    std::string bar = "";
+    bar += "-";
+    for(int bar_idx = 0; bar_idx < 80; bar_idx++) bar += "-";
+
     gmsh::initialize();
-    gmsh::open("TestMesh.msh");
+    gmsh::open(argv[1]);
+
     std::vector<std::string> model_names;
     gmsh::model::list(model_names);
     std::cout << "Listing model names: " << std::endl;
@@ -15,41 +22,87 @@ int main (int argc, char **argv)
         std::cout << model_names[ii] << std::endl;
     }
 
-    gmsh::vectorpair physical_groups;
-    gmsh::model::getPhysicalGroups(physical_groups);
-    std::cout << "These are my physical groups:" << std::endl;
-    for (int ii = 0; ii < physical_groups.size(); ii++)
+    // Entities
+    gmsh::vectorpair dimTags;
+    gmsh::model::getEntities(dimTags);
+    std::cout << "Entities" << std::endl;
+    std::cout << "This is basically me reading you the" << std::endl;
+    std::cout << "geo file" << std::endl;
+    std::cout << bar << std::endl;
+    for(int ent_idx = 0; ent_idx < dimTags.size(); ent_idx++)
     {
-        std::cout << "Dimension: " << physical_groups[ii].first << ", Tag: " << physical_groups[ii].second << std::endl;
+
+        int ent_dim = dimTags[ent_idx].first;
+        int ent_tag = dimTags[ent_idx].second;
+        std::cout << "Entity Number: " << ent_idx << std::endl;
+        std::cout << "\tDimension: " << ent_dim << std::endl;
+        std::cout << "\tTag: " << ent_tag << std::endl;
+        std::cout << std::endl;
+        std::string ent_name;
+
+        gmsh::model::setEntityName(
+            ent_dim,
+            ent_tag,
+            ent_name);
+
+        std::cout << "\tName: " << ent_name << std::endl;
+
+        std::vector<int> phys_tags;
+        gmsh::model::getPhysicalGroupsForEntity(
+            ent_dim,
+            ent_tag,
+            phys_tags);
+        std::cout << "\tPhysical Groups: ";
+        for(int phys_idx = 0; phys_idx < phys_tags.size(); phys_idx++)
+        {
+            std::cout << phys_tags[phys_idx] << "\t";
+        }
+        std::cout << std::endl;
+        std::string ent_type;
+        gmsh::model::getType(ent_dim,ent_tag,ent_type);
+        std::cout << "\tType: " << ent_type << std::endl;
+
+        int parent_dim,parent_tag;
+        gmsh::model::getParent(ent_dim,ent_tag,parent_dim,parent_tag);
+        std::cout << "\tParent Info:" << std::endl;
+        if(parent_dim==-1 || parent_tag==-1)
+        {
+            std::cout << "\t\tEntity has no parent" << std::endl;
+        }
+        else
+        {
+            std::cout << "\t\tDimension: " << parent_dim << std::endl;
+            std::cout << "\t\tTag: " << parent_tag << std::endl;
+        }
+
+
+        std::cout << std::endl;
     }
-    std::vector<int> ele_types;
-    std::vector<std::vector<std::size_t> > ele_tags;
-    std::vector<std::vector<std::size_t> > node_tags;
-    gmsh::model::mesh::getElements(ele_types,ele_tags,node_tags);
-    for (int jj = 0; jj < ele_types.size(); jj++)
+
+    gmsh::model::getPhysicalGroups(dimTags);
+    std::cout << "Physical Groups" << std::endl;
+    std::cout << "This is basically me reading you the" << std::endl;
+    std::cout << "important mesh information." << std::endl;
+    std::cout << bar << std::endl;
+    for(int phys_idx = 0; phys_idx < dimTags.size(); phys_idx++)
     {
-        std::cout << "Type No. " << jj << " is " << ele_types[jj] << std::endl;
-        std::string type_name;
-        int type_dim;
-        int type_ord;
-        int type_n_nodes;
-        std::vector<double> type_coords;
-        gmsh::model::mesh::getElementProperties(ele_types[jj], type_name, type_dim, type_ord, type_n_nodes, type_coords);
-        std::cout << "Name:\t" << type_name << "\n\tDimension:\t" << type_dim << "\n\tOrder:\t" << type_ord << "\n\tNodes:\t" << type_n_nodes << "\n\tReference Coordinates:"<< std::endl;
-        for (int nn = 0; nn < type_n_nodes; nn++)
-        {
-            std::cout << "\t";
-            for (int dd = 0; dd < type_dim; dd++)
-            {
-                std::cout << "\t" << type_coords[nn*type_dim+dd];
-            }
-            std::cout << std::endl;
-        }
-        for (int kk = 0; kk < ele_tags[jj].size(); kk++)
-        {
-            std::cout << "\t" << ele_tags[jj][kk] << std::endl;
-        }
+        std::cout << "Physical Group Number: " << phys_idx;
+        std::cout << std::endl;
+        int phys_dim = dimTags[phys_idx].first;
+        int phys_tag = dimTags[phys_idx].second;
+        std::cout << "\tDimension: " << phys_dim << std::endl;
+        std::cout << "\tTag: " << phys_tag << std::endl;;
+        std::string phys_name;
+        gmsh::model::getPhysicalName(
+            phys_dim,
+            phys_tag,
+            phys_name);
+        std::cout << "\tPhysical Group Name: ";
+        std::cout << phys_name << std::endl;
+
     }
+
+    // A matrix
     Eigen::MatrixXd m(2,2);
     m(0,0) = 1;
     m(0,1) = 2;
